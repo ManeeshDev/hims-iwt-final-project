63eff3a41d38bd7e934670ad9d9eb0073d66a42c
hansajith18 <hansajith18@gmail.com>
2022-05-16 10:19:08 +0530

    Config file & Admin index

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php include_once(dirname(__FILE__) .  '/../includes/config.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= APP_NAME ?> Admin </title>
+
+    <link href="css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="css/custom.css" rel="stylesheet" />
+</head>
+
+<body>
+    <?php include_once(dirname(__FILE__) . '/navigation.php') ?>
+</body>
+
+</html>
