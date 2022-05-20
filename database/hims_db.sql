dae3efc11886ba1cc65b1e2a62acc4fa2391236e
IT21156410-FonsekaMMNH <it21156410@my.sliit.lk>
2022-05-20 23:21:11 +0530

    Database User tables and config files

--- database/hims_db.sql
+++ database/hims_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2022 at 01:04 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: May 20, 2022 at 07:50 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,9 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agent`
+--
+
+DROP TABLE IF EXISTS `agent`;
+CREATE TABLE `agent` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `branch` varchar(100) NOT NULL,
+  `rank` varchar(100) NOT NULL,
+  `basic_salary` double NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `buy_policy`
 --
 
+DROP TABLE IF EXISTS `buy_policy`;
 CREATE TABLE `buy_policy` (
   `client_id` int(11) NOT NULL,
   `policy_id` int(11) NOT NULL,
@@ -37,9 +67,31 @@ CREATE TABLE `buy_policy` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED DEFAULT NULL,
+  `agent_id` int(10) UNSIGNED DEFAULT NULL,
+  `state` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `street` varchar(100) DEFAULT NULL,
+  `postal_code` varchar(50) DEFAULT NULL,
+  `dob` date DEFAULT NULL,
+  `marital_state` enum('single','married','divorced') DEFAULT NULL,
+  `gender` enum('male','female') DEFAULT NULL,
+  `registered_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `policy`
 --
 
+DROP TABLE IF EXISTS `policy`;
 CREATE TABLE `policy` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -50,9 +102,49 @@ CREATE TABLE `policy` (
   `term` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nic` varchar(12) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_contact`
+--
+
+DROP TABLE IF EXISTS `user_contact`;
+CREATE TABLE `user_contact` (
+  `user_id` int(11) NOT NULL,
+  `phone_number` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `agent`
+--
+ALTER TABLE `agent`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `buy_policy`
@@ -62,19 +154,57 @@ ALTER TABLE `buy_policy`
   ADD KEY `buy_policy_policy_fk` (`policy_id`);
 
 --
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `policy`
 --
 ALTER TABLE `policy`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nic` (`nic`);
+
+--
+-- Indexes for table `user_contact`
+--
+ALTER TABLE `user_contact`
+  ADD PRIMARY KEY (`user_id`,`phone_number`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `agent`
+--
+ALTER TABLE `agent`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `policy`
 --
 ALTER TABLE `policy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- includes/config.php
+++ includes/config.php
@@ -1,3 +1,14 @@
 <?php
 
 define('APP_NAME', 'HIMS');
+
+const HOSTNAME  = "localhost";
+const USERNAME  = "root";
+const PASSWORD  = "";
+const DATABASE  = "hims_db";
+
+$conn = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+if (!$conn) {
+    die("Connection Failed:" . mysqli_connect_error());
+}
