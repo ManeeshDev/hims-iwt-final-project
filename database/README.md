5862db3ed0dc427b3e5701a9b78f09342b177528
IT21169908-PrashanthaKGM <it21169908@my.sliit.lk>
2022-05-20 17:01:44 +0530

    Make database directory and database file with (policy & buy_policy) tables.

---
+++ database/README.md
@@ -0,0 +1,27 @@
+### Make a new Database (`hims_db`) in your MySQL-DBMS (`phpMyAdmin`).
+### Import tables by `hims_db.sql` from `project/database` directory.
+
+
+```
+> make new database by project db-name.
+>
+> > http://localhost/phpmyadmin/
+
+> import hims_db.sql file to your created database.
+>
+> > http://localhost/phpmyadmin/index.php?route=/database/import&db=hims_db
+```
+
+
+    After choose the hims_db.sql file from directory.
+    (ex: `C:\xampp\htdocs\hims-iwt-final-project\database`)
+
+    Then unchecked this tic ->>
+
+    Other options:
+        [] `Enable foreign key checks`
+    Format:
+        SQL
+
+    Then Click ->> `Go`
+
---
+++ database/hims_db.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2022 at 01:04 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hims_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buy_policy`
+--
+
+CREATE TABLE `buy_policy` (
+  `client_id` int(11) NOT NULL,
+  `policy_id` int(11) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `policy`
+--
+
+CREATE TABLE `policy` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `coverage` varchar(255) DEFAULT NULL,
+  `age_limit` int(11) DEFAULT NULL,
+  `benefit` varchar(255) DEFAULT NULL,
+  `per_month` float DEFAULT NULL,
+  `term` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buy_policy`
+--
+ALTER TABLE `buy_policy`
+  ADD PRIMARY KEY (`client_id`,`policy_id`),
+  ADD KEY `buy_policy_policy_fk` (`policy_id`);
+
+--
+-- Indexes for table `policy`
+--
+ALTER TABLE `policy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `policy`
+--
+ALTER TABLE `policy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buy_policy`
+--
+ALTER TABLE `buy_policy`
+  ADD CONSTRAINT `buy_policy_client_fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `buy_policy_policy_fk` FOREIGN KEY (`policy_id`) REFERENCES `policy` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
