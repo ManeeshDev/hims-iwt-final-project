f56d982346a31e69be38fe04bfcafeba46721f30
IT21169908-PrashanthaKGM <it21169908@my.sliit.lk>
2022-05-23 13:57:50 +0530

    about page

--- about.php
+++ about.php
@@ -27,9 +27,57 @@ include_once(dirname(__FILE__) .  '/includes/config.php');
     <!-- ===================================== END NAV-BAR ===================================== -->
     <main>
         <!-- =========== ABOUT-US =============================== -->
-        <div style="margin:200px">
-            <h1>About Us</h1>
+        <section>
+            <div class="about-section">
+                <div class="inner-container">
+                    <h1>About Us</h1>
+                    <p class="text">
+                    We are your partner in total health and wellness, 24/7. <br><br>
+                    At Hims, we're more than a health insurance company. We are your partner in total health and wellness. And we’re here for you 24/7 – caring for your body and mind. <br><br>
+
+                    As a global health service company, Hims's mission is to improve the health, well-being, and peace of mind of those we serve by making health care simple, affordable, and predictable. <br><br>
+
+                    <span> We make it easy to get care – letting you choose how, when, and where you want it.</span> <br>
+                    <span>We make health care more affordable by partnering with providers who provide quality, cost-effective care.</span> <br>
+                    <span> Our goal is to provide you with comprehensive health care coverage with “no surprises.” </span> <br> <br>
+
+                    Our values are the core of our culture. Our values guide how all 74,000 of us around the world work together, serve our customers, patients, clients, communities, and deliver on our mission.
+                    </p>
+                    <div class="we-are">
+                        <div class="elements">
+                            <div class="we-are-img">
+                                <img src="./images/about/ab-1.png" alt="we-are-img">
+                            </div>
+                            <span>We care deeply about our customers, patients, and coworkers</span>
+                        </div>
+                        <div class="elements">
+                            <div class="we-are-img">
+                                <img src="./images/about/ab-2.png" alt="we-are-img">
+                            </div>
+                            <span>We partner, collaborate, and keep our promises</span>
+                        </div>
+                        <div class="elements">
+                            <div class="we-are-img">
+                                <img src="./images/about/ab-3.png" alt="we-are-img">
+                            </div>
+                           <span>We innovate and adapt</span>
+                        </div>
+                        <div class="elements">
+                            <div class="we-are-img">
+                                <img src="./images/about/ab-4.png" alt="we-are-img">
+                            </div>
+                            <span>We act with speed and purpose</span>
+                        </div>
+                        <div class="elements">
+                            <div class="we-are-img">
+                                <img src="./images/about/ab-5.png" alt="we-are-img">
+                            </div>
+                            <span>We create a better future–together</span>
+                        </div>
+                    </div>
+            </div>
         </div>
+        </section>
         <!-- =========== END ABOUT-US =============================== -->
 
     </main>
